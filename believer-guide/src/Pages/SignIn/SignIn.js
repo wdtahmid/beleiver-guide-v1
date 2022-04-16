ec6d202fff278b75c1e706e4a71a93326a9f968a
Tahmid Hasan <wdtahmid@gmail.com>
2022-04-17 01:27:33 +0600

    all pages added

---
+++ believer-guide/src/Pages/SignIn/SignIn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignIn = () => {
+    return (
+        <div>
+            <h2>This is Sign In Page</h2>
+        </div>
+    );
+};
+
+export default SignIn;
---
+++ believer-guide/src/Pages/SignUp/SignUp.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignUp = () => {
+    return (
+        <div>
+            <h2>This is Sign Up Page</h2>
+        </div>
+    );
+};
+
+export default SignUp;
