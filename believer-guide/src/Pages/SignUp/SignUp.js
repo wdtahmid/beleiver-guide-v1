bc860b12d9737ae2ce7fdde248a73c03bbce9be8
Tahmid Hasan <wdtahmid@gmail.com>
2022-04-17 14:09:20 +0600

    style signup form, changed bg image

--- believer-guide/src/Pages/SignUp/SignUp.js
+++ believer-guide/src/Pages/SignUp/SignUp.js
@@ -10,20 +10,21 @@ const SignUp = () => {
     }
     return (
         <div className="signup-container vh-full flex flex-col justify-items-start items-center h-screen">
-            <div className="signup pt-20">
-                <h2>Please Register</h2>
-                <div className="form">
-                    <form onSubmit={handleSignUp}>
+            <div className="signup pt-20 w-96 mx-auto">
+                <h2 className='text-4xl text-white mb-5'>Please Register</h2>
+                <div className="form w-full bg-black bg-opacity-20 p-5 rounded-md">
+                    <form onSubmit={handleSignUp} className='flex flex-col gap-3'>
                         <div className="input-group">
-                            <input type="text" name="name" id="name" placeholder='Your Name Here' />
+                            <input className='w-full p-3 rounded-md border-2 border-yellow-400 text-black' type="text" name="name" id="name" placeholder='Your Name Here' />
                         </div>
                         <div className="input-group">
-                            <input type="email" name="email" id="email" placeholder='Your Email Here' required />
+                            <input className='w-full p-3 rounded-md border-b-2 border-yellow-400 text-black' type="email" name="email" id="email" placeholder='Your Email Here' required />
                         </div>
                         <div className="input-group">
-                            <input type="password" name="password" id="password" placeholder='Your Password' required />
+                            <input className='w-full p-3 rounded-md border-b-2 border-yellow-400 text-black' type="password" name="password" id="password" placeholder='Your Password' required />
                         </div>
                     </form>
+                    
                 </div>
             </div>
         </div>
